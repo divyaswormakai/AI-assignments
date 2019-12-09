faa73de4639220a3140508478beb97a9adf6f9b4
Divyaswor Makai <makaidivya@gmail.com>
2019-12-09 23:14:57 +0545

    final Tree changes

--- Assignment 2/men vs cannibal.py
+++ Assignment 2/men vs cannibal.py
@@ -2,9 +2,6 @@ from anytree import Node,RenderTree
 from anytree.exporter import DotExporter
 from graphviz import render
 
-start = [3,3,0,[]]
-final = [0,0,1,[]]
-goal = [0,0,1]
 checkRange = range(0,4)
 
 def BFSMethod():
@@ -23,7 +20,6 @@ def BFSMethod():
 		currCannibal = currState[1]
 		currSide = currState[2]
 		parentState =currState[3]
-		print(currState)
 		if(currMen == 0 and currCannibal ==0 and currSide==1):	#Condition Complete
 			completeLength = counter
 			complete = True
@@ -55,7 +51,7 @@ def BFSMethod():
 						temp.append([currMen,currCannibal+2,side,counter])
 
 					for val in temp:
-						if(val[0]>=0 and val[0]<=3 and val[1]>=0 and val[1]<=3):		#if the added value is within range
+						if(val[0] in checkRange and val[1] in checkRange):		#if the added value is within range
 							valSet=[val[0],val[1],val[2]]
 							if(valSet not in compareSet):				#if the state already exists in the tree
 								stateSet.append(val)
@@ -63,70 +59,51 @@ def BFSMethod():
 
 		counter+=1
 
-# Building the tree
+	# Building the tree
 	print("---------------------")
 	counter = 1
 	nodes =[]
 	nodes.append(Node(str([3,3,0,0])))
-	while counter<completeLength+1:
+	while counter<=completeLength:
 		parentNode = nodes[(stateSet[counter][3])]
 		nodes.append(Node(str(stateSet[counter]),parent = parentNode))
 		counter+=1
-	# for pre, fill, node in RenderTree(nodes[0]):
-	# 	print("%s%s" %(pre,node.name))
 
-	fileLoc = "E:/7th Sem/Artificial Intelligence/Assignments/BFSTree.dot"
+	fileLoc = "E:/7th Sem/Artificial Intelligence/Assignments/Assignment 2/BFSTree.dot"
 	DotExporter(nodes[0]).to_dotfile(fileLoc)
 	render('dot','png',fileLoc)
 
+	# Building the path only for the BFS tree
+	states = []
+	count = completeLength
+	currState = stateSet[count]
+	currCompareValues = [currState[0],currState[1],currState[2]]
+	while currCompareValues!=[3,3,0]:
+		states.append(currCompareValues)
+		count = currState[3]
+		currState = stateSet[count]
+		currCompareValues = [currState[0],currState[1],currState[2]]
 
-def DFSrecur(state,parentList):
-	if(state == goal):
-		parentList.append(state)
-		PrintAllStates(parentList)
-		print("-------------------\n")
-		DFSComplete=True
-		return 0
-	else:
-		men = state[0]
-		cannibal = state[1]
-		side = state[2]
-		parent = state[3]
+	states.append([3,3,0]) #It wont be added due to the while condition
+	states.reverse()
+	nodes =[]
+	nodes.append(Node(str([3,3,0])))
+	count =1
+	while count < len(states):
+		print(states[count])
+		parentNode = nodes[count-1]
+		nodes.append(Node(str(states[count]),parent = parentNode))
+		count+=1
 
-		if(men in checkRange and cannibal in checkRange and CheckPossible(men,cannibal)):
-			if(state in parentList):
-				return 0
-				
-			else:
-				parentList.append(state)
-				if(side==0):
-					side = 1-side
-					DFS([men-1,cannibal,side],parentList)
-					DFS([men-2,cannibal,side],parentList)
-					DFS([men-1,cannibal-1,side],parentList)
-					DFS([men,cannibal-1,side],parentList)
-					DFS([men,cannibal-2,side],parentList)
+	fileLoc = "E:/7th Sem/Artificial Intelligence/Assignments/Assignment 2/BFSTreeFinal.dot"
+	DotExporter(nodes[0]).to_dotfile(fileLoc)
+	render('dot','png',fileLoc)
 
-				elif(side ==1):
-					side = 1-side
-					DFS([men+1,cannibal,side],parentList)
-					DFS([men+2,cannibal,side],parentList)
-					DFS([men+1,cannibal+1,side],parentList)
-					DFS([men,cannibal+1,side],parentList)
-					DFS([men,cannibal+2,side],parentList)
-		else:
-			return 0
-
-def PrintAllStates(parentList):
-	for i in parentList:
-		print(i)
-
-def CheckPossible(men,cannibal):
-	if(men==0 or men == 3 or men == cannibal):
-		return True
-	return False
-
-def DFS():       #use a stack to help track remaining nodes
+#In DFS we take values from the end of the list because we tried to mimic that of stack 
+#I didnt use stack beacuse we would have to use pop operation to get the top value which would take the value
+#out of stack which was necessary for the next steps
+#visited boolean helped to identify if some of the nodes were already visited or not
+def DFS():       
 	state = [3,3,0,0,False]				#[men,cannibal,side,parent,visited]
 	complete = False
 	stateSet = []
@@ -186,7 +163,7 @@ def DFS():       #use a stack to help track remaining nodes
 								stateSet.append(val)
 
 
-# Building the tree
+	# Building the tree
 	print("---------------------")
 	counter = 1
 	nodes =[]
@@ -197,23 +174,100 @@ def DFS():       #use a stack to help track remaining nodes
 		parentNode = nodes[(stateSet[counter][3])]
 		nodes.append(Node(str([stateSet[counter][0],stateSet[counter][1],stateSet[counter][2]]),parent = parentNode))
 		counter+=1
-	# for pre, fill, node in RenderTree(nodes[0]):
-	# 	print("%s%s" %(pre,node.name))
 
-	fileLoc = "E:/7th Sem/Artificial Intelligence/Assignments/DFSTree.dot"
+	fileLoc = "E:/7th Sem/Artificial Intelligence/Assignments/Assignment 2/DFSTree.dot"
 	DotExporter(nodes[0]).to_dotfile(fileLoc)
 	render('dot','png',fileLoc)
+
+	# Building the path only for the DFS tree
+	states = []
+	count = len(stateSet)-1
+	currState = stateSet[count]
+	currCompareValues = [currState[0],currState[1],currState[2]]
+	while currCompareValues!=[3,3,0]:
+		states.append(currCompareValues)
+		count = currState[3]
+		currState = stateSet[count]
+		currCompareValues = [currState[0],currState[1],currState[2]]
+
+	states.append([3,3,0]) #It wont be added due to the while condition
+	states.reverse()
+	nodes =[]
+	nodes.append(Node(str([3,3,0])))
+	count =1
+	while count < len(states):
+		parentNode = nodes[count-1]
+		nodes.append(Node(str(states[count]),parent = parentNode))
+		count+=1
+
+	fileLoc = "E:/7th Sem/Artificial Intelligence/Assignments/Assignment 2/DFSTreeFinal.dot"
+	DotExporter(nodes[0]).to_dotfile(fileLoc)
+	render('dot','png',fileLoc)
+
+######################################################################################
+######################################################################################	
+##########				Main funtion 										##########
 
 if __name__ == '__main__':
 	print("BFS METHOD")
 	BFSMethod()
 
+	#Gives some result I couldn't solve so not using
 	# print("\n\nDFS METHOD")
 	# DFSrecur([3,3,0],[])
 
 	print("DFS MEthod")
 	DFS()
 
+######################################################################################
+######################################################################################	
+##########				   Main funtion Ends								##########
+
 	
+def DFSrecur(state,parentList):
+	if(state == goal):
+		parentList.append(state)
+		PrintAllStates(parentList)
+		print("-------------------\n")
+		DFSComplete=True
+		return 0
+	else:
+		men = state[0]
+		cannibal = state[1]
+		side = state[2]
+		parent = state[3]
+
+		if(men in checkRange and cannibal in checkRange and CheckPossible(men,cannibal)):
+			if(state in parentList):
+				return 0
+				
+			else:
+				parentList.append(state)
+				if(side==0):
+					side = 1-side
+					DFS([men-1,cannibal,side],parentList)
+					DFS([men-2,cannibal,side],parentList)
+					DFS([men-1,cannibal-1,side],parentList)
+					DFS([men,cannibal-1,side],parentList)
+					DFS([men,cannibal-2,side],parentList)
+
+				elif(side ==1):
+					side = 1-side
+					DFS([men+1,cannibal,side],parentList)
+					DFS([men+2,cannibal,side],parentList)
+					DFS([men+1,cannibal+1,side],parentList)
+					DFS([men,cannibal+1,side],parentList)
+					DFS([men,cannibal+2,side],parentList)
+		else:
+			return 0
+
+def PrintAllStates(parentList):
+	for i in parentList:
+		print(i)
+
+def CheckPossible(men,cannibal):
+	if(men==0 or men == 3 or men == cannibal):
+		return True
+	return False
 
 
